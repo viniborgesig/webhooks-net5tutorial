748d24dfde672436a786a9011fd6e43e03f52a82
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-18 15:09:33 -0300

    feat(webhook registration api): post endpoint logic - persisting our resource

--- AirlineWeb/Controllers/WebhookSubscriptionsController.cs
+++ AirlineWeb/Controllers/WebhookSubscriptionsController.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Linq;
 using AirlineWeb.Data;
 using AirlineWeb.Dtos;
 using AirlineWeb.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AirlineWeb.Controllers
@@ -10,15 +13,42 @@ namespace AirlineWeb.Controllers
     public class WebhookSubscriptionsController : ControllerBase
     {
         private readonly AirlineDbContext _context;
+        private readonly IMapper _mapper;
 
-        public WebhookSubscriptionsController(AirlineDbContext context)
+        public WebhookSubscriptionsController(AirlineDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        // public ActionResult<WebhookSubscriptionReadDto> CreateSubscription(WebhookSubscriptionCreateDto webhookSubscription)
-        // {
-            
-        // }
+
+        [HttpPost]
+        public ActionResult<WebhookSubscriptionReadDto> CreateSubscription(WebhookSubscriptionCreateDto webhookSubscriptionCreateDto)
+        {
+             var webhookSubscription = _context.WebhookSubscriptions
+                .FirstOrDefault(ws => ws.WebhookURI == webhookSubscriptionCreateDto.WebhookURI);
+
+            if (webhookSubscription is null)
+            {
+                webhookSubscription = _mapper.Map<WebhookSubscription>(webhookSubscriptionCreateDto);
+
+                webhookSubscription.Secret = Guid.NewGuid().ToString();
+                webhookSubscription.WebhookPublisher = "LATAM Airlines";
+
+                try
+                {
+                    _context.WebhookSubscriptions.Add(webhookSubscription);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+                var webhookSubscriptionReadDto = _mapper.Map<WebhookSubscriptionReadDto>(webhookSubscription);
+            }
+
+            return NoContent();
+        }
     }
 }
--- AirlineWeb/Startup.cs
+++ AirlineWeb/Startup.cs
@@ -30,12 +30,13 @@ namespace AirlineWeb
         {
 
             services.AddDbContext<AirlineDbContext>(opt => opt.UseSqlServer
-            (Configuration.GetConnectionString("AirlineConnection")));
+                (Configuration.GetConnectionString("AirlineConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AirlineWeb", Version = "v1" });
             });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
