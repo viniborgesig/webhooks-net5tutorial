20c58acdd73d9b68a35912ef16304cfe8e6b99f6
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-18 16:46:51 -0300

    feat(flight details airline api): flight detail data transfer objects (dtos)

---
+++ AirlineWeb/Dtos/FlightDetailCreateDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineWeb.Dtos
+{
+    public class FlightDetailCreateDto
+    {
+        [Required]
+        public string FlightCode { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
