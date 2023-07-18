14f36f4e39d691af44d5eb16614750f52c670168
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-18 13:20:08 -0300

    feat(webhook registration api): model & db context

---
+++ AirlineWeb/Data/AirlineDbContext.cs
@@ -0,0 +1,15 @@
+using AirlineWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineWeb.Data
+{
+    public class AirlineDbContext : DbContext
+    {
+        public AirlineDbContext(DbContextOptions<AirlineDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; }
+    }
+}
--- AirlineWeb/Startup.cs
+++ AirlineWeb/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AirlineWeb.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace AirlineWeb
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<AirlineDbContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("AirlineConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
