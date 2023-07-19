4b759f5617a980c5b9aae333540b0be75bdc0bb2
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-19 17:11:07 -0300

    feat(webhook send agent): data transfer objects for send agent

---
+++ AirlineSendAgent/Dtos/NotificationMessageDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AirlineSendAgent.Dtos
+{
+    public class NotificationMessageDto
+    {
+        public string Id { get; }
+        public string WebhookType { get; set; }
+        public string FlightCode { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal NewPrice { get; set; }
+    }
+}
