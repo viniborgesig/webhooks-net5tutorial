751ac2201551f31f960997f214ace214d7dfbfdf
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-19 17:06:07 -0300

    feat(webhook send agent): config layer, models and db context for send agent

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,4 +1,7 @@
 ﻿using AirlineSendAgent.App;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +15,8 @@ namespace AirlineSendAgent
                 .ConfigureServices((context, services) =>
                 {
                     services.AddSingleton<IAppHost, AppHost>();
+                    services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer
+                        (context.Configuration.GetConnectionString("AirlineConnection")));
                 }).Build();
 
             host.Services.GetService<IAppHost>().Run();
