2d2f42eb340303187982470fbe6ee4b36c87fb1f
Vinícius Borges <viniciusborgesbr@outlook.com>
2023-07-19 17:44:37 -0300

    feat(webhook send agent): bringing it together - finalizing the send agent

--- AirlineSendAgent/App/AppHost.cs
+++ AirlineSendAgent/App/AppHost.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using AirlineSendAgent.Client;
+using AirlineSendAgent.Data;
+using AirlineSendAgent.Dtos;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -6,6 +12,15 @@ namespace AirlineSendAgent.App
 {
     public class AppHost : IAppHost
     {
+        private readonly SendAgentDbContext _context;
+        private readonly IWebhookClient _webhookClient;
+
+        public AppHost(SendAgentDbContext context, IWebhookClient webhookClient)
+        {
+            _context = context;
+            _webhookClient = webhookClient;
+        }
+
         public void Run()
         {
             var connectionFactory = new ConnectionFactory()
@@ -33,6 +48,31 @@ namespace AirlineSendAgent.App
                 consumer.Received += async (ModuleHandle, EventArgs) =>
                 {
                     Console.WriteLine("Evento foi disparado...");
+
+                    var body = EventArgs.Body;
+                    var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+                    var notificationMessageDto = JsonSerializer.Deserialize<NotificationMessageDto>(notificationMessage);
+
+                    var webhookToSend = new FlightDetailChangePayloadDto
+                    {
+                        WebhookURI = string.Empty,
+                        WebhookType = notificationMessageDto.WebhookType,
+                        FlightCode = notificationMessageDto.FlightCode,
+                        Publisher = string.Empty,
+                        Secret = string.Empty,
+                        OldPrice = notificationMessageDto.OldPrice,
+                        NewPrice = notificationMessageDto.NewPrice
+                    };
+
+                    foreach (var webhookSubscription in _context.WebhookSubscriptions
+                        .Where(ws => ws.WebhookType.Equals(notificationMessageDto.WebhookType)))
+                    {
+                        webhookToSend.WebhookURI = webhookSubscription.WebhookURI;
+                        webhookToSend.Secret = webhookSubscription.Secret;
+                        webhookToSend.Publisher = webhookSubscription.WebhookPublisher;
+
+                        await _webhookClient.SendWebhookNotification(webhookToSend);
+                    }
                 };
 
                 channel.BasicConsume(queue: queueName,
